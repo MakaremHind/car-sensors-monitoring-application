6b76daf6c1bc4cecdb36b330add337a214787eb2
Hind <hind.a.makarem@gmail.com>
2024-11-15 16:16:45 +0100

    Set up MongoDB connection using Mongoose

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+const connectDB = require("./config/database");
+connectDB();
