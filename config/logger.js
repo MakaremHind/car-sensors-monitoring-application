2e431b608490dfac92dffb1e434038e3ce59a315
Hind <hind.a.makarem@gmail.com>
2024-12-01 21:22:24 +0100

    Create logger.js and set up winston for logging with different levels

---
+++ config/logger.js
@@ -0,0 +1,19 @@
+// logger.js
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'debug',  // Default log level is debug, can be changed based on the environment
+  format: winston.format.combine(
+    winston.format.colorize(),  // Add colors for logs in the console
+    winston.format.timestamp(), // Add timestamp for each log
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} ${level}: ${message}`;  // Custom log format
+    })
+  ),
+  transports: [
+    new winston.transports.Console({ format: winston.format.simple() }), // Log to console
+    // You can add more transports here, e.g., to log to files or external services
+  ],
+});
+
+module.exports = logger;
