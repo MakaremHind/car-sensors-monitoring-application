4ad512cb54a9c61a1c755524c69c56f405c4f6bd
Hind <hind.a.makarem@gmail.com>
2024-12-01 22:03:25 +0100

    Add logging to API routes in CarRoutes.js for request details

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -1,6 +1,8 @@
+// CarRoutes.js
 const express = require("express");
 const { check } = require("express-validator");
 const carController = require("../controllers/carController");
+const logger = require("../logger");  // Import the logger
 
 const router = express.Router();
 
@@ -13,12 +15,31 @@ router.post(
     check("sensors.*.type", "Sensor type is required").not().isEmpty(),
     check("sensors.*.value", "Sensor value must be a number").isNumeric(),
   ],
+  (req, res, next) => {
+    logger.info("Received POST request for /api/cars with data:", req.body);
+    next();
+  },
   carController.createCar
 );
 
-router.get("/", carController.getCars);
-router.get("/:id", carController.getCarById);
-router.put("/:id", carController.updateCar);
-router.delete("/:id", carController.deleteCar);
+router.get("/", (req, res, next) => {
+  logger.info("Received GET request for /api/cars");
+  next();
+}, carController.getCars);
+
+router.get("/:id", (req, res, next) => {
+  logger.info(`Received GET request for /api/cars/${req.params.id}`);
+  next();
+}, carController.getCarById);
+
+router.put("/:id", (req, res, next) => {
+  logger.info(`Received PUT request for /api/cars/${req.params.id} with data:`, req.body);
+  next();
+}, carController.updateCar);
+
+router.delete("/:id", (req, res, next) => {
+  logger.info(`Received DELETE request for /api/cars/${req.params.id}`);
+  next();
+}, carController.deleteCar);
 
 module.exports = router;
